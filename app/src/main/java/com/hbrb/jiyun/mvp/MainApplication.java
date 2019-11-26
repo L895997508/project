9e348c7ffa6b1e340c1cfb82d7337f0ecc1c2826
ericwind19891113 <546451263@qq.com>
2019-11-26 10:29:47 +0800

    第二

--- app/src/main/java/com/hbrb/jiyun/mvp/MainApplication.java
+++ app/src/main/java/com/hbrb/jiyun/mvp/MainApplication.java
@@ -18,7 +18,7 @@ public class MainApplication extends Application{
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage25554522 */ false);
+    SoLoader.init(this, /* native exopackage222 */ false);
 
     /**
      * 注册APPID
